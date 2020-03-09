0f151c286d25ddc12d52f7b9d9a7fb21d4dfc1ee
mazhihui <mazhihui@jd.com>
2020-03-09 09:03:54 +0800

    update

---
+++ src/main/java/com/mzh/study/unit/chess/AbstractChess.java
@@ -0,0 +1,17 @@
+package com.mzh.study.unit.chess;
+
+/**
+ * Created by mazhihui on 2020/3/9.
+ */
+public class AbstractChess {
+    static{
+        System.out.println("父类静态代码块");
+    }
+    public AbstractChess(){
+        System.out.println("父类构造方法");
+    }
+    {
+        System.out.println("父类构造代码块");
+    }
+
+}
--- src/main/java/com/mzh/study/unit/chess/ChessUnitFactory.java
+++ src/main/java/com/mzh/study/unit/chess/ChessUnitFactory.java
@@ -6,17 +6,24 @@ import java.util.Map;
 /**
  * Created by mazhihui on 2020/3/8.
  */
-public class ChessUnitFactory{
+public class ChessUnitFactory extends AbstractChess{
     private static final Map<Integer,ChessUnit> chessMap = new HashMap<>();
 
     static{
         chessMap.put(1,new ChessUnit(1,"車",ChessUnit.Color.RED));
         chessMap.put(2,new ChessUnit(2,"馬",ChessUnit.Color.RED));
-        System.out.println("我是static代码块");
+        System.out.println("子类静态代码块");
+    }
+    public ChessUnitFactory(){
+        super();
+        System.out.println("子类构造方法");
     }
 
     public static ChessUnit getChessUnit(Integer chessId){
         return chessMap.get(chessId);
     }
 
+    public static void main(String[] args) {
+        new ChessUnitFactory();
+    }
 }
